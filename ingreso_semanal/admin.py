769d87364a80bee0707bf69fbd74aa5601d39081
Manuel Riquelme <clonbg@gmail.com>
2022-07-27 19:53:52 +0200

    tabla ingreso_semanal

--- ingreso_semanal/admin.py
+++ ingreso_semanal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ingresoSemanal
 
 # Register your models here.
+
+@admin.register(ingresoSemanal)
+class ingresoSemanalAdmin(admin.ModelAdmin):
+    list_display=['dia_inicio','dia_fin','taxista','total_efectivo_semana','total_apps_semana','total_tpv_semana','varios_semana']
+    list_filter=['dia_inicio','dia_fin','taxista']
--- ingreso_semanal/models.py
+++ ingreso_semanal/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from taxistas.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+
+
+class ingresoSemanal(models.Model):
+    dia_inicio = models.DateField(unique=True)
+    dia_fin = models.DateField(default=now,unique=True)
+    imagen_semana = models.ImageField(
+        upload_to='imgs/%Y/%m/%d/', null=True, blank=True)
+    total_efectivo_semana = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    total_apps_semana = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    total_tpv_semana = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    varios_semana = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(-1000000)
+    ],null=True, blank=True)
+    taxista = models.ForeignKey(User, on_delete=models.CASCADE, null=True,blank=True)
+
+    def __str__(self):
+        return f"<Ingreso semanal {self.taxista}"
