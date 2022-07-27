ddca52157f9087ebbd9f407e49da465d96edfa3e
Manuel Riquelme <clonbg@gmail.com>
2022-07-27 19:41:25 +0200

    tabla ingreso_diario

--- ingreso_diario/admin.py
+++ ingreso_diario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ingresoDiario
 
 # Register your models here.
+
+@admin.register(ingresoDiario)
+class ingresoDiarioAdmin(admin.ModelAdmin):
+    list_display=['dia','taxista','total_efectivo','total_apps','total_tpv','varios']
+    list_filter=['dia','taxista']
--- ingreso_diario/models.py
+++ ingreso_diario/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from taxistas.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+
+
+class ingresoDiario(models.Model):
+    dia = models.DateField(default=now, unique=True)
+    imagen = models.ImageField(
+        upload_to='imgs/%Y/%m/%d/', null=True, blank=True)
+    total_efectivo = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    total_apps = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    total_tpv = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(1)
+    ],null=True, blank=True)
+    varios = models.FloatField(validators=[
+        MaxValueValidator(1000000),
+        MinValueValidator(-1000000)
+    ],null=True, blank=True)
+    taxista = models.ForeignKey(User, on_delete=models.CASCADE, null=True,blank=True)
+
+    def __str__(self):
+        return f"<Ingreso diario {self.taxista}"
