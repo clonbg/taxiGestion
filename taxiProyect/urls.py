4736d8dc55a6a6c1c4fb965adb1ca9c9b672f29e
Manuel Riquelme <clonbg@gmail.com>
2022-08-01 17:19:06 +0200

    comentarios

--- taxiProyect/urls.py
+++ taxiProyect/urls.py
@@ -7,12 +7,35 @@ urlpatterns = [
     path('licencias/',include('licencias.urls')),
     path('ingreso_diario/',include('ingreso_diario.urls')),
     path('ingreso_semanal/',include('ingreso_semanal.urls')),
-    path('taxistas/', include('djoser.urls')),
+    path('djoser/', include('djoser.urls.jwt')),
+    path('djoser/', include('djoser.urls')),
 ]
 
 
 """
-API
-taxistas/registros/
-licencias/create
+# djoser
+
+/users/
+/users/me/
+/users/confirm/
+/users/resend_activation/
+/users/set_password/
+/users/reset_password/
+/users/reset_password_confirm/
+/users/set_username/
+/users/reset_username/
+/users/reset_username_confirm/
+/token/login/ (Token Based Authentication)
+/token/logout/ (Token Based Authentication)
+/jwt/create/ (JSON Web Token Authentication)
+/jwt/refresh/ (JSON Web Token Authentication)
+/jwt/verify/ (JSON Web Token Authentication)
+
+# taxistas
+
+taxistas/registro/
+
+# licencias
+
+licencias/create/
 """
--- taxistas/urls.py
+++ taxistas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# taxistas/
 urlpatterns=[
     path('',views.HelloTaxistasView.as_view(),name='Hello_taxista'),
     path('registro/',views.UserCreateView.as_view(),name='registro')
