7d892c0621d83df233c6ae300fc74fcae51cb983
Manuel <clonbg@gmail.com>
2022-08-05 16:07:53 +0200

    Ya se ven las imágenes

--- taxiProyect/urls.py
+++ taxiProyect/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,7 +17,7 @@ urlpatterns = [
     path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     #path('djoser/', include('djoser.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 """
