ba1cff769869dfd8d5e2b72c79a4d807d029712e
Manuel <clonbg@gmail.com>
2022-08-12 16:49:10 +0200

    meta rutas protegidas

--- Taxi-gestión/src/router/index.js
+++ Taxi-gestión/src/router/index.js
@@ -1,17 +1,29 @@
-import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { route } from "quasar/wrappers";
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
 
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-    history: createHistory(process.env.VUE_ROUTER_BASE)
-  })
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
 
-  return Router
-})
+  Router.beforeEach((to, from, next) => {
+    console.log(to.meta.auth);
+    next();
+  });
+
+  return Router;
+});
--- Taxi-gestión/src/router/routes.js
+++ Taxi-gestión/src/router/routes.js
@@ -1,25 +1,39 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'diario', component: () => import('pages/IngresoDiario.vue') },
-      { path: 'semanal', component: () => import('pages/IngresoSemanal.vue') },
-      { path: 'graficas', component: () => import('src/pages/GraficasPage.vue') },
-
-    ]
+      {
+        path: "",
+        component: () => import("pages/IndexPage.vue"),
+        meta: { auth: true },
+      },
+      {
+        path: "diario",
+        component: () => import("pages/IngresoDiario.vue"),
+        meta: { auth: true },
+      },
+      {
+        path: "semanal",
+        component: () => import("pages/IngresoSemanal.vue"),
+        meta: { auth: true },
+      },
+      {
+        path: "graficas",
+        component: () => import("src/pages/GraficasPage.vue"),
+        meta: { auth: true },
+      },
+    ],
   },
 
   {
-    path: '/login',
-    component: () => import('pages/LoginPage.vue')
+    path: "/login",
+    component: () => import("pages/LoginPage.vue"),
   },
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
