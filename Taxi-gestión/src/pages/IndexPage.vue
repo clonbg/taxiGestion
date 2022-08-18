e1e0a3d583d45efcde861b2e217d5e09170c2b68
Manuel Riquelme <clonbg@gmail.com>
2022-08-18 09:03:26 +0200

    dni es válido y no está en la bd

--- Taxi-gestión/src/pages/IndexPage.vue
+++ Taxi-gestión/src/pages/IndexPage.vue
@@ -1,9 +1,16 @@
 <template>
   <q-page>
     <div class="flex flex-center">
-      <form class="form q-my-xl" @submit.prevent="subir()" style="min-width:600px">
-        <q-img :src="`${taxiStore.urlServer}${taxiStore.user.foto}`" class="imagen q-ma-xl" :ratio="16 / 9"><template
-            v-slot:error>
+      <form
+        class="form q-my-xl"
+        @submit.prevent="subir()"
+        style="min-width: 600px"
+      >
+        <q-img
+          :src="`${taxiStore.urlServer}${taxiStore.user.foto}`"
+          class="imagen q-ma-xl"
+          :ratio="16 / 9"
+          ><template v-slot:error>
             <div class="absolute-full flex flex-center bg-negative text-white">
               No se puede cargar la imagen
             </div>
@@ -14,18 +21,47 @@
             <q-icon name="attach_file" />
           </template>
         </q-file>
-        <q-input standout v-model="taxiStore.user.nombre" label="Nombre" dense class="q-my-lg"
-          :rules="[val => val && val.length >= 3 && val.length <= 20 || 'Entre 3 y 20 carácteres']" />
-        <q-input standout v-model="taxiStore.user.apellidos" label="Apellidos" dense class="q-my-lg"
-          :rules="[val => val && val.length >= 3 && val.length <= 50 || 'Entre 3 y 50 carácteres']" />
-        <q-input standout v-model="taxiStore.user.dni" label="DNI" dense class="q-my-lg"
-          :rules="[val => val && val.length >= 3 && val.length <= 50 || 'No es válido']" />
-        <q-btn class="form-submit" type="submit" :disable=saveState>Save</q-btn>
+        <q-input
+          standout
+          v-model="taxiStore.user.nombre"
+          label="Nombre"
+          dense
+          class="q-my-md"
+          :rules="[
+            (val) =>
+              (val && val.length >= 3 && val.length <= 20) ||
+              'Entre 3 y 20 carácteres',
+          ]"
+        />
+        <q-input
+          standout
+          v-model="taxiStore.user.apellidos"
+          label="Apellidos"
+          dense
+          class="q-my-md"
+          :rules="[
+            (val) =>
+              (val && val.length >= 3 && val.length <= 50) ||
+              'Entre 3 y 50 carácteres',
+          ]"
+        />
+        <q-input
+          standout
+          v-model="taxiStore.user.dni"
+          label="DNI"
+          dense
+          class="q-my-md"
+          :rules="[
+            (val) =>
+              (val && !nif(val) && nifBd(val)) || 'No es válido o ya existe',
+          ]"
+        />
+        <q-btn class="form-submit" type="submit" :disable="saveState"
+          >Save</q-btn
+        >
       </form>
     </div>
-    <p>seguir con el formulario y las validaciones y notificación error y ok
-      dni,existe, es del tamaño y noestá en la bd
-    </p>
+    <p>seguir con el formulario y las validaciones y notificación error y ok</p>
     <pre>{{ taxiStore.user }}</pre>
     <pre>{{ `${taxiStore.urlServer}${taxiStore.user.foto}` }}</pre>
     <pre>{{ taxiStore.listaUsuarios }}</pre>
@@ -35,28 +71,35 @@
 <script setup>
 import { useTaxiStore } from "../stores/taxi-store";
 import { onMounted, ref, computed } from "vue";
-import { api } from '../boot/axios'
+import { api } from "../boot/axios";
 
 const taxiStore = useTaxiStore();
 
-const file = ref(null)
+const file = ref(null);
 
 const getUser = async () => {
   await taxiStore.usuario();
 };
 
 const saveState = computed(() => {
-  if (taxiStore.user.nombre.length < 3 || taxiStore.user.nombre.length > 20 || taxiStore.user.apellidos.length < 3 || taxiStore.user.apellidos.length > 50) {
-    return true
+  if (
+    taxiStore.user.nombre.length < 3 ||
+    taxiStore.user.nombre.length > 20 ||
+    taxiStore.user.apellidos.length < 3 ||
+    taxiStore.user.apellidos.length > 50 ||
+    nif(taxiStore.user.dni) ||
+    !nifBd(taxiStore.user.dni)
+  ) {
+    return true;
   }
-  return false
-})
+  return false;
+});
 
 const nif = (dni) => {
-  var numero
-  var letr
-  var letra
-  var expresion_regular_dni
+  var numero;
+  var letr;
+  var letra;
+  var expresion_regular_dni;
 
   expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
 
@@ -64,52 +107,62 @@ const nif = (dni) => {
     numero = dni.substr(0, dni.length - 1);
     letr = dni.substr(dni.length - 1, 1);
     numero = numero % 23;
-    letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
+    letra = "TRWAGMYFPDXBNJZSQVHLCKET";
     letra = letra.substring(numero, numero + 1);
     if (letra != letr.toUpperCase()) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   } else {
-    return true
+    return true;
   }
-}
+};
+
+const nifBd = (dni) => {
+  var usersBd = taxiStore.listaUsuarios
+    .filter((item) => item.id !== taxiStore.user.id)
+    .filter((item) => item.dni.toUpperCase() == dni.toUpperCase());
+  if (usersBd.length == 0) {
+    return true;
+  }
+  return false;
+};
 
 const subir = async () => {
   await taxiStore.refresToken();
   if (taxiStore.access_token) {
     let axiosConfig = {
       headers: {
-        Authorization: `Bearer ${taxiStore.access_token}`
+        Authorization: `Bearer ${taxiStore.access_token}`,
       },
     };
     var formData = new FormData();
-    formData.append('dni', taxiStore.user.dni)
-    formData.append("nombre", taxiStore.user.nombre)
-    formData.append("apellidos", taxiStore.user.apellidos)
-    formData.append("sueldo", taxiStore.user.sueldo)
+    formData.append("dni", taxiStore.user.dni);
+    formData.append("nombre", taxiStore.user.nombre);
+    formData.append("apellidos", taxiStore.user.apellidos);
+    formData.append("sueldo", taxiStore.user.sueldo);
     if (file.value) {
-      formData.append("foto", file.value)
+      formData.append("foto", file.value);
     }
     if (taxiStore.user.licencia) {
-      formData.append("licencia_id", parseInt(taxiStore.user.licencia.id))
+      formData.append("licencia_id", parseInt(taxiStore.user.licencia.id));
     }
-    formData.append("email", taxiStore.user.email)
-    formData.append("phone_number", taxiStore.user.phone_number)
+    formData.append("email", taxiStore.user.email);
+    formData.append("phone_number", taxiStore.user.phone_number);
 
-    console.log(axiosConfig)
+    console.log(axiosConfig);
     await api
       .put(`/taxistas/${taxiStore.user.id}/`, formData, axiosConfig)
       .then(async (res) => {
-        file.value = null
-        getUser()
-        taxiStore.letrero.nombre = taxiStore.user.nombre
-        taxiStore.letrero.apellidos = taxiStore.user.apellidos
-        console.log(res)
+        file.value = null;
+        getUser();
+        taxiStore.letrero.nombre = taxiStore.user.nombre;
+        taxiStore.letrero.apellidos = taxiStore.user.apellidoS;
+        console.log(res);
       })
       .catch((err) => {
-        console.log(err.response.data);
+        console.log(err.response);
       });
   }
 };
