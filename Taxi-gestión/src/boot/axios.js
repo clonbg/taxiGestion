ff32bde318376ca78998a32037c9bd61bd072d60
clonbg <clonbg@gmail.com>
2022-09-27 17:56:35 +0200

    añadido /api a axios

--- Taxi-gestión/src/boot/axios.js
+++ Taxi-gestión/src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000/api",
   withCredentials: true,
 });
 
