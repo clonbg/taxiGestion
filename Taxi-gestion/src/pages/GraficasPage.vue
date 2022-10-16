82b749b530f2054478f84c8023905cd6768d8db4
clonbg <clonbg@gmail.com>
2022-10-16 17:23:53 +0200

    Falta la data de los taxistas

--- Taxi-gestion/src/pages/GraficasPage.vue
+++ Taxi-gestion/src/pages/GraficasPage.vue
@@ -5,16 +5,12 @@
         <q-select
           class="q-mr-xs"
           filled
-          :model-value="taxista1"
-          use-input
-          hide-selected
-          fill-input
-          input-debounce="0"
+          v-model="taxista1"
           :options="options1"
           @filter="filterFn1"
-          @input-value="setModel1"
           style="width: 250px; padding-bottom: 32px"
           label="Taxista 1"
+          behavior="menu"
         >
           <template v-slot:no-option>
             <q-item>
@@ -23,17 +19,40 @@
           </template>
         </q-select>
         <q-select
+          class="q-mr-xs"
           filled
-          :model-value="taxista2"
-          use-input
-          hide-selected
-          fill-input
-          input-debounce="0"
+          v-model="taxista2"
           :options="options2"
           @filter="filterFn2"
-          @input-value="setModel2"
           style="width: 250px; padding-bottom: 32px"
           label="Taxista 2"
+          behavior="menu"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey"> No results </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+        <q-input
+          class="q-mr-xs"
+          standout
+          v-model.number="anyo"
+          type="number"
+          label="Año"
+          filled
+          :rules="[
+            ((val) => val && !isNaN(val) && val >= 2022 && val <= 2200) ||
+              'Año no válido',
+          ]"
+        />
+        <q-select
+          filled
+          v-model="mes"
+          :options="optionsMeses"
+          style="width: 250px; padding-bottom: 32px"
+          label="Mes"
+          behavior="menu"
         >
           <template v-slot:no-option>
             <q-item>
@@ -43,7 +62,7 @@
         </q-select>
       </div>
     </div>
-    <div>
+    <div class="q-mr-md">
       <chart
         :series="series"
         :options="chartOptions"
@@ -68,10 +87,16 @@ const taxista2 = ref(null);
 let stringOptions2 = [];
 const options2 = ref(stringOptions2);
 const componentkey = ref(0);
+const anyo = ref(new Date().getFullYear());
+const optionsMeses =
+  "Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre".split(
+    "_"
+  );
+const mes = ref(optionsMeses[new Date().getMonth()]);
 var series = [
   {
     name: "",
-    data: [31, 40, 28, 51, 42, 109, 100],
+    data: [],
   },
   {
     name: "",
@@ -92,15 +117,7 @@ const chartOptions = {
   },
   xaxis: {
     type: "datetime",
-    categories: [
-      "2018-09-19",
-      "2018-09-20",
-      "2018-09-21",
-      "2018-09-22",
-      "2018-09-23",
-      "2018-09-24",
-      "2018-09-25",
-    ],
+    categories: [],
   },
   tooltip: {
     x: {
@@ -125,7 +142,16 @@ const listaTaxistas = () => {
     });
 };
 
+const getDiarios = async () => {
+  await taxiStore.get_ingresos_diarios();
+  // for del mes con los días
+  // si hay entrada diaria del taxista 1 o 2 se añade a su data
+  // si no añadimos un cero
+};
+
 watchEffect(() => {
+  getDiarios();
+  console.log(taxiStore.diarios);
   if (typeof taxista1.value === "string") {
     series[0].name = taxista1.value;
     // Traer las entradas diarias que coinciden con este taxista, sumarlas cada día y devolverlo en array
@@ -134,7 +160,28 @@ watchEffect(() => {
     series[1].name = taxista2.value;
     // Traer las entradas diarias que coinciden con este taxista, sumarlas cada día y devolverlo en array
   }
-
+  if (
+    anyo.value &&
+    !isNaN(anyo.value) &&
+    anyo.value >= 2022 &&
+    anyo.value <= 2200
+  ) {
+    //días tiene el mes
+    let num_dias_mes = new Date(
+      anyo.value,
+      optionsMeses.indexOf(mes.value) + 1,
+      0
+    ).getDate();
+    chartOptions.xaxis.categories = [];
+    for (var i = 1; i <= num_dias_mes; i++) {
+      let item = `${anyo.value}-${
+        optionsMeses.indexOf(mes.value) >= 9
+          ? optionsMeses.indexOf(mes.value) + 1
+          : "0" + (optionsMeses.indexOf(mes.value) + 1).toString()
+      }-${i > 9 ? i : "0" + i.toString()}`;
+      chartOptions.xaxis.categories.push(item);
+    }
+  } else chartOptions.xaxis.categories = [];
   if (stringOptions2 != [] || stringOptions1 != []) {
     listaTaxistas();
   }
@@ -157,14 +204,6 @@ const filterFn2 = (val, update, abort) => {
       (v) => v.toLocaleLowerCase().indexOf(needle) > -1
     );
   });
-};
-
-const setModel1 = (val) => {
-  taxista1.value = val;
-};
-
-const setModel2 = (val) => {
-  taxista2.value = val;
 };
 
 onMounted(() => {
