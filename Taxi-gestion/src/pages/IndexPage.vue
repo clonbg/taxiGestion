e779b1a03dc271ec6bf3232dbee91ee9c259cf89
clonbg <clonbg@gmail.com>
2023-01-10 11:38:14 +0100

    Depurados errores y format

--- Taxi-gestion/src/pages/IndexPage.vue
+++ Taxi-gestion/src/pages/IndexPage.vue
@@ -2,139 +2,75 @@
   <q-page>
     <div class="flex flex-center q-ma-sm">
       <div class="row">
-      <q-form
-        class="form"
-        @submit.prevent="subir()"
-        autocorrect="off"
-        autocapitalize="off"
-        autocomplete="off"
-        spellcheck="false"
-      >
-        <q-img
-          :src="`${taxiStore.urlServer}${taxiStore.user.foto}`"
-          class="imagen q-mx-xl"
-          :ratio="16 / 9"
-        >
-          <template v-slot:error>
-            <div class="absolute-full flex flex-center bg-negative text-white">
-              No se puede cargar la imagen
+        <q-form class="form" @submit.prevent="subir()" autocorrect="off" autocapitalize="off" autocomplete="off"
+          spellcheck="false">
+          <q-img :src="`${taxiStore.urlServer}${taxiStore.user.foto}`" class="imagen q-mx-xl" :ratio="16 / 9">
+            <template v-slot:error>
+              <div class="absolute-full flex flex-center bg-negative text-white">
+                No se puede cargar la imagen
+              </div>
+            </template>
+          </q-img>
+          <div class="row">
+            <div class="col-12">
+              <q-file v-model="file" label="Cambie su foto (5Mb Max)" filled class="q-my-xl" max-file-size="5242880"
+                accept=".jpg,.jpeg,.png" @rejected="onRejected" :filter="validarFile">
+                <template v-slot:prepend>
+                  <q-icon name="attach_file" />
+                </template>
+              </q-file>
             </div>
-          </template>
-        </q-img>
-        <div class="row">
-          <div class="col-12">
-            <q-file
-              v-model="file"
-              label="Cambie su foto (5Mb Max)"
-              filled
-              class="q-my-xl"
-              max-file-size="5242880"
-              accept=".jpg,.jpeg,.png"
-              @rejected="onRejected"
-              :filter="validarFile"
-            >
-              <template v-slot:prepend>
-                <q-icon name="attach_file" />
-              </template>
-            </q-file>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <q-input
-              standout
-              v-model="taxiStore.user.nombre"
-              label="Nombre"
-              dense
-              :rules="[
+          <div class="row">
+            <div class="col-12">
+              <q-input standout v-model="taxiStore.user.nombre" label="Nombre" dense :rules="[
                 (val) =>
                   (val && val.length >= 3 && val.length <= 20) ||
                   'Entre 3 y 20 carácteres',
-              ]"
-            />
+              ]" />
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <q-input
-              standout
-              v-model="taxiStore.user.apellidos"
-              label="Apellidos"
-              dense
-              :rules="[
+          <div class="row">
+            <div class="col-12">
+              <q-input standout v-model="taxiStore.user.apellidos" label="Apellidos" dense :rules="[
                 (val) =>
                   (val && val.length >= 3 && val.length <= 50) ||
                   'Entre 3 y 50 carácteres',
-              ]"
-            />
+              ]" />
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <q-input
-              standout
-              v-model="taxiStore.user.dni"
-              label="DNI"
-              dense
-              :rules="[
+          <div class="row">
+            <div class="col-12">
+              <q-input standout v-model="taxiStore.user.dni" label="DNI" dense :rules="[
                 (val) =>
                   (val && !nif(val) && nifBd(val)) ||
                   'No es válido o ya existe',
-              ]"
-            />
+              ]" />
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-4">
-            <q-input
-              v-model="taxiStore.user.sueldo"
-              standout
-              dense
-              label="Sueldo"
-              suffix="%"
-              disable
-              readonly
-            />
+          <div class="row">
+            <div class="col-4">
+              <q-input v-model="taxiStore.user.sueldo" standout dense label="Sueldo" suffix="%" disable readonly />
+            </div>
+            <div class="col-1"></div>
+            <div class="col-7">
+              <q-input standout v-model="licencia" label="Número de licencia" dense disable readonly />
+            </div>
           </div>
-          <div class="col-1"></div>
-          <div class="col-7">
-            <q-input
-              standout
-              v-model="licencia"
-              label="Número de licencia"
-              dense
-              disable
-              readonly
-            />
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <q-input
-              class="q-mt-md"
-              standout
-              v-model="taxiStore.user.email"
-              label="Email"
-              type="email"
-              dense
-              :rules="[
+          <div class="row">
+            <div class="col-12">
+              <q-input class="q-mt-md" standout v-model="taxiStore.user.email" label="Email" type="email" dense :rules="[
                 (val) =>
                   (val &&
                     /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val) &&
                     emailBd(val)) ||
                   'Email no válido o ya existente',
-              ]"
-            />
+              ]" />
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <q-input
-              standout
-              v-model="taxiStore.user.phone_number"
-              label="Número de teléfono"
-              dense
-              :rules="[
+          <div class="row">
+            <div class="col-12">
+              <q-input standout v-model="taxiStore.user.phone_number" label="Número de teléfono" dense :rules="[
                 (val) =>
                   (val &&
                     !isNaN(val) &&
@@ -142,31 +78,18 @@
                     val.length <= 11 &&
                     tlfBd(val)) ||
                   'Número de teléfono no válido o existente',
-              ]"
-            />
+              ]" />
+            </div>
           </div>
-        </div>
-        <q-btn
-          class="form-submit"
-          type="submit"
-          :disable="saveState"
-          :color="saveState ? 'red' : 'green'"
-          :loading="loading[0]"
-          @click="progress = true"
-          >Guardar</q-btn
-        >
-        <q-btn
-          class="form-submit q-ml-md"
-          @click="
-            getUser();
-            file = null;
-          "
-          color="primary"
-          >Cancelar</q-btn
-        >
-      </q-form>
+          <q-btn class="form-submit" type="submit" :disable="saveState" :color="saveState ? 'red' : 'green'"
+            :loading="loading[0]" @click="progress = true">Guardar</q-btn>
+          <q-btn class="form-submit q-ml-md" @click="
+  getUser();
+file = null;
+          " color="primary">Cancelar</q-btn>
+        </q-form>
+      </div>
     </div>
-  </div>
   </q-page>
 </template>
 
@@ -328,7 +251,7 @@ const subir = async () => {
 
     await api
       .put(`/taxistas/${taxiStore.user.id}/`, formData, axiosConfig)
-      .then(async (res) => {
+      .then(async () => {
         simulateProgress(0);
       })
       .catch((err) => {
